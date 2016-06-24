8f4a5e927cfe3ffd2629b97c146854a426ebdebe
Fabian Sperrle <fabian.sperrle@gmail.com>
2016-06-24 14:58:45 +0200

    Fix formula...
    woops.

--- src/distance/EuclidianDistance.java
+++ src/distance/EuclidianDistance.java
@@ -1,34 +1,55 @@
 package distance;
 
+import java.util.Arrays;
+
 /**
  * Created by fabian on 24.06.2016.
  */
 public class EuclidianDistance {
 
     public static double getEuclidianDistance(double[] a, double[] b) {
+        double[] weights = new double[a.length];
+        Arrays.fill(weights, 1);
+        return getEuclidianDistance(a, b, weights, a.length);
+    }
+
+    public static double getEuclidianDistance(double[] a, double[] b, double[] weights, int dim) {
         double sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += Math.pow(a[i] + b[i], 2);
+        for (int i = 0; i < dim; i++) {
+            System.out.println("dim = " + dim);
+            sum += weights[i] * Math.pow(a[i] - b[i], 2);
         }
 
         return Math.sqrt(sum);
     }
 
     public static double getEuclidianDistance(int[] a, int[] b) {
+        double[] weights = new double[a.length];
+        Arrays.fill(weights, 1);
+        return getEuclidianDistance(a, b, weights);
+    }
+
+    public static double getEuclidianDistance(int[] a, int[] b, double[] weights) {
         double sum = 0;
         for (int i = 0; i < a.length; i++) {
-            sum += Math.pow(a[i] + b[i], 2);
+            sum += weights[i] * Math.pow(a[i] - b[i], 2);
         }
 
         return Math.sqrt(sum);
     }
 
     public static double getEuclidianDistance(int[][][] a, int[][][] b) {
+        double[] weights = new double[a.length];
+        Arrays.fill(weights, 1);
+        return getEuclidianDistance(a, b, weights);
+    }
+
+    public static double getEuclidianDistance(int[][][] a, int[][][] b, double[] weights) {
         double sum = 0;
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
                 for (int k = 0; k < a.length; k++) {
-                    sum += Math.pow(a[i][j][k] + b[i][j][k], 2);
+                    sum += weights[i] * Math.pow(a[i][j][k] - b[i][j][k], 2);
                 }
             }
         }
@@ -41,7 +62,7 @@ public class EuclidianDistance {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
                 for (int k = 0; k < a.length; k++) {
-                    sum += Math.pow(a[i][j][k] + b[i][j][k], 2);
+                    sum += Math.pow(a[i][j][k] - b[i][j][k], 2);
                 }
             }
         }
